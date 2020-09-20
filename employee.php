f68434adf194bd79c1936da8740a6e05cb32f282
dsijnvsd <dsijnvsd@gamil.com>
2020-09-20 13:25:58 -0600

    employee.php changed

--- employee.php
+++ employee.php
@@ -21,12 +21,18 @@
              $result = $conn->query($sql);
 
              if ($result->num_rows > 0) {
+               echo "<form action="employee_checkbox.php" method="post">";
                  // output data of each row
                  while($row = $result->fetch_assoc()) {
                      $fname= $row["First_Name"];
                      $lname=$row["Last_Name"];
-                     echo "<li>$fname $lname</li>";
+                     $sql_1 = "SELECT id FROM Employee WHERE First_Name='$fname' AND Last_Name='$lname'";
+                     $id = $connect->query($sql_1);
+                    //  echo "<li>$fname $lname</li>";
+                     echo "<input type="checkbox" name="chkl[ ]" value="$id">'$fname $lname'<br />  "
                   }
+                echo "<button type="submit" name="Add New Employee" value="Add New Employee">";
+                echo "<button type="submit" name="Add New Employee" value="Add New Employee">";
               }
             }
             $conn->close();
---
+++ employee_checkbox.php
@@ -0,0 +1,18 @@
+
+<?php
+  include 'header.php';
+  include 'sql.php';
+  include 'employee.php';
+
+
+  $checkbox1 = $_POST['chkl'] ;  
+if ($_POST["Submit" ]=="Submit")  
+{  
+for ($i=0; $i<sizeof ($checkbox1);$i++) {  
+$query="INSERT INTO employee (name) VALUES ('".$checkboxl[$i]. "')";  
+mysql_query($query) or die(mysql_error());  
+} 
+echo "Record is inserted.";
+}  
+?>
+
